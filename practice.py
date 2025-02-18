284bff1e876390bca58cd00d4d2ce069e017c809
Ian To <onlyian4981@gmail.com>
2025-02-18 10:38:48 -0800

    exercise1.py: Move io.py to exercise1.py
    Rename io.py to exercise1.py. Also create practice.py
    
    Signed-off-by: Ian To <onlyian4981@gmail.com>

---
+++ practice.py
@@ -0,0 +1,18 @@
+'''
+c = 900
+d = int(900 / 300)
+
+print(c)
+print(d)
+
+
+num1 = int(input("What number do you want?\n"))
+print(num1 * 2)
+'''
+def name_print() -> None:
+    first_name = input("Input your first name\n")
+    last_name = input("Input your last name\n")
+    
+    print(first_name + " "  + last_name)
+
+name_print()
